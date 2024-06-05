d3866f4d1b8de103db8b107ee9ac4dd4fc7efa03
satheesh <jegatheesh.c@grouppal.in>
2024-06-05 12:08:26 +0530

    Initial Commit

---
+++ AppleSign/AppleSignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSignApp.swift
+//  AppleSign
+//
+//  Created by APPLE on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleSignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
